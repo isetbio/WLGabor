0b762648325407ee6332d7dfd93ee8db65e6de01
Brian Wandell <wandell@stanford.edu>
2018-03-07 11:18:21 -0800

    ZL and BW adjusted the names of the files and the variables.  Getting ready to discriminate.

---
+++ tutorials/t_ccDiscriminate.m
@@ -0,0 +1,106 @@
+function t_ccDiscriminate
+%% cc = contrast classification
+%
+%   Create Gabor stimuli with different contrasts.  Run an SVM to see
+%   if we can tell them apart.
+%
+% ZL, SCIEN STANFORD, 2018
+
+%%
+ieInit
+
+%%
+nFrames   = 100;   % Number of temporal samples
+numFrameNoStmls = 100;
+numFrameTotal = nFrames + numFrameNoStmls;
+nTrials = 100;
+stmlType = {'Yes', 'No'};   % Stimulus or no stimulus
+
+%% Calculate the total number of absorptions
+
+% <trials,row,col,time>
+absorptions = ccAbsorptions(stmlType{1}, nTrials);
+%{
+% Look at the movie from a trial
+thisTrial = 1;
+trialData = squeeze(absorptions(thisTrial,:,:,:));
+ieMovie(trialData);
+%}
+
+%% Create a vector of the mean absorptions on each trial
+%
+% We place the data into one big matrix with <space, trials>
+meanAbsorptions = mean(absorptions, 4);
+%{
+trialData = squeeze(meanAbsorptions(1,:,:));
+vcNewGraphWin;
+imagesc(trialData); colormap(gray); colorbar; axis image
+%}
+
+% We want to put each vector of the cone absorptions in a trial into a
+% row of the stimulus matrix. Later we will add the no stimulus trials with their label.
+frameStmlsReshp = permute(meanAbsorptions,[2 3 1]);
+frameStmlsReshp = RGB2XWFormat(frameStmlsReshp)';
+% size(frameStmlsReshp)
+
+%%  ZL
+frameNoStmls = squeeze(ccframeStmlsCreate(numFrameNoStmls, stmlType{2}, nTrials));
+[nTrials, row, col, numFrameNoStmls] = size(frameNoStmls);
+meanFrameNoStmls = mean(frameNoStmls, 4);
+frameNoStmlsReshp = reshape(meanFrameNoStmls, [row * col, nTrials]);
+frameNoStmlsReshp = frameNoStmlsReshp';
+%% plot mean results
+meanStmlsPlot(meanFrameStmls);
+
+hold on;
+
+meanStmlsPlot(meanFrameNoStmls);
+
+%%
+dataStmls = [frameStmlsReshp;frameNoStmlsReshp];
+classStmls = cell(numFrameTotal,1);
+for i = 1 : nFrames
+    classStmls{i} = stmlType{1};
+    classStmls{i + nFrames} = stmlType{2};
+end
+%classStmls{1:numFrameStmls} = stmlType{1};
+%classStmls{numFrameStmls + 1 : end} = stmlType{2};
+
+%%
+kernelFunction = 'rbf';
+standardizeSVMpredictors = false;
+
+kFold = 10;
+
+
+%svm = fitcsvm(dataStmls, classStmls, 'KernelFunction',kernelFunction,'KernelScale','auto', 'standardize', standardizeSVMpredictors);
+svm = fitcsvm(dataStmls, classStmls);
+CVSVM = crossval(svm,'KFold',kFold);
+percentCorrect = 1 - kfoldLoss(CVSVM,'lossfun','classiferror','mode','individual');
+stdErr = std(percentCorrect)/sqrt(kFold);
+percentCorrect = mean(percentCorrect);
+
+%% Plot result
+sv = svm.SupportVectors;
+figure
+gscatter(dataStmls(:,1),dataStmls(:,2),classStmls)
+hold on
+plot(sv(:,1),sv(:,2),'ko','MarkerSize',10)
+legend('Harmonic Stimulus','No Harmonic Stimulus','Support Vector')
+hold off
+%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ utilities/ccAbsorptions.m
@@ -0,0 +1,127 @@
+function absorptions = ccAbsorptions(stmlType, nTrials)
+% Calculate the total cone absorptions for multiple trials  
+%
+% Description:
+%  Create a harmonic stimulus, a cone mosaic, and calculate the total
+%  number of cone absorptions from the stimulus.
+%
+%
+% Inputs:
+%
+% Optional key/value pairs:
+%   emDuration         - total simulation time (s)
+%   sampTime           - integrate time unit (s)
+%   nTrials            - number of trials
+%   moduleLength       - change according to sampTime
+%   fovDegs            - Set accordingly
+%   resamplingFactors  - Cone mosaic for hex case
+%
+% Outputs:
+%   absorptions - matrix of numbers for the cone mosaic absorptions
+%
+% ZL, SCIEN, 2018
+%
+% Based on t_fixationalEyeMovementsTypes
+%
+% See also:
+
+
+%%  ZL to implement the parameters
+
+% p = inputParser;
+
+%% Generate eye movements
+
+% Generate eye movement data for 2 1-second long trials, with a
+% sample time of 1 millisecond.
+emDuration = 1; sampTime = 1/100;
+
+% Do not compute velocity of eye movements
+compVelocity = false;
+usePar = false;
+
+% Random seed to be used in all eye movement compute() calls
+ranSeed = 1;
+
+% Initialize object
+fixEMobj = fixationalEM();
+
+% First case: No micro-saccades, only drift
+% Set all params to their default value
+fixEMobj.setDefaultParams();
+fixEMobj.microSaccadeType = 'stats based';
+fixEMobj.randomSeed = ranSeed;
+
+fixEMobj.compute(emDuration, sampTime, nTrials, ...
+    compVelocity, 'useParfor', usePar);
+
+%%  Generate oisequence
+% Two scenes, for oiFixed and oiModulated
+scene = cell(1,2);
+% oiModulated harmonic parameters
+tparams(2) = harmonicP;
+tparams(2).freq = 4;
+tparams(2).GaborFlag = 0.2;
+
+% oiFixed has the same parameters, but zero contrast
+tparams(1) = tparams(2);
+tparams(1).contrast = 0;
+
+if (strcmp(stmlType, 'No'))   % No stimulus
+    tparams(2).contrast = 0;
+end
+% Create the harmonic scenes
+for ii=1:2
+    scene{ii} = sceneCreate('harmonic',tparams(ii));
+end
+
+
+% Compute optical images from the scene
+OIs = cell(1, 2);
+oi = oiCreate;
+for ii = 1:2
+    OIs{ii} = oiCompute(oi,scene{ii});
+end
+
+integrateTimeOI = 0.01;
+moduleLength = emDuration / integrateTimeOI;
+modulation = ieScale(fspecial('gaussian',[1,moduleLength],10),0,.5);
+sampleTimes = ((1: length(modulation))-1)*integrateTimeOI;
+ois = oiSequence(OIs{1}, OIs{2}, sampleTimes, modulation, ...
+    'composition', 'blend');
+%ois.visualize('movie illuminance');
+%%
+fovDegs = 1;
+TimeIntegrat = 1/100;
+
+cm = coneMosaic('fovDegs', fovDegs);
+cm.integrationTime = TimeIntegrat;
+
+%% Generate cone mosaic
+% cone mosaic parms
+% fovDegs = 1;
+% TimeIntegrat = 1/100;
+% resamplingFactors = 3; %[1 3 6 13];
+%
+% % % Instantiate a hex mosaic with a specific resampling factor
+% cm = coneMosaicHex(resamplingFactors, 'fovDegs', fovDegs);
+% cm.integrationTime = TimeIntegrat;
+%legends{numel(legends)+1} = sprintf('resampling: %2.0f ms', resamplingFactors(iSampleIndex));
+
+%% Compute the number of eye movements for this integration time and oiSequence
+eyeMovementsPerTrial = ois.maxEyeMovementsNumGivenIntegrationTime(cm.integrationTime);
+fixEMobj.computeForConeMosaic(cm, eyeMovementsPerTrial, ...
+    'nTrials', nTrials, ...
+    'computeVelocity', true, ...
+    'rSeed', 1);
+%%
+cm.emPositions = fixEMobj.emPos;
+[absorptions, ~, ~, ~ ] = cm.compute(ois);
+cm.emPositions = fixEMobj.emPos;
+
+%% Might need to change the window function
+%cm.window;
+%% Extract  based on number of frame
+absorptions = absorptions;
+
+end
